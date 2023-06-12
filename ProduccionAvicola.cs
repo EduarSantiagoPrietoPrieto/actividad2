4cb858fa3562d6185df5ab9fba8e1905885629dc
caduenas <duenascarlos9@gmail.com>
2023-06-11 19:39:33 -0500

    Final

---
+++ ProduccionAvicola.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+public class ProduccionAvicola
+{
+
+    public void iniciar()
+	{
+		string continua = "";
+		do
+		{
+            double proceso = 0;
+            double calculo = 0;
+            Console.WriteLine("--------------------------------");
+            Console.WriteLine("--------------------------------");
+            Console.WriteLine("--------------------------------");
+            Console.WriteLine("Por favor digite la cantidad de gallinas");
+            double gallinas = double.Parse(Console.ReadLine());
+            Console.WriteLine("Por favor digite la cantidad de dias");
+            double dias = double.Parse(Console.ReadLine());
+            for (int i = 1; i <= dias; i++)
+            {
+                calculo = Math.Pow(gallinas, i);
+                calculo = calculo / calcularFactorial(i);
+                proceso = proceso + calculo;
+            }
+            Console.WriteLine("El resultado es: " + (proceso + 1));
+            Console.WriteLine("Desea continuar s/n");
+            continua = Console.ReadLine();
+        } while(continua.Contains("s"))
+    }
+	public double calcularFactorial(double numero)
+	{
+		if (numero == 0)
+		{
+			return 1;
+		}
+        else
+        {
+            return numero * calcularFactorial(numero - 1);
+        }
+    }
+}
--- estacion_climatica.cs
+++ estacion_climatica.cs
@@ -1,27 +1,69 @@
-﻿using System;
-
-public class estacion_climatica
+﻿public class estacion_climatica
 {
-
     public void iniciar()
-	{
-        string[] subcadenas;
-        do {
-            Console.WriteLine("---------------------------");
-            Console.WriteLine("---------------------------");
-            Console.WriteLine("Por favor ingrese los valores maximo y minimo de la siguiente forma 15,20");
-            Console.WriteLine("---------------------------");
-            Console.WriteLine("---------------------------");
-            Console.WriteLine("Para teminar el ingreso de datos ingrese los valoes de la siguiente forma 0,0");
-            Console.WriteLine("---------------------------");
-            Console.WriteLine("---------------------------");
-            string cadena = Console.ReadLine();
-            subcadenas = cadena.Split(',');
-            foreach (string subcadena in subcadenas)
+    {
+        String continua = "";
+        do
+        {
+            string[] subcadenas;
+            List<int> menor = new List<int>();
+            List<int> mayor = new List<int>();
+            int diasFallidos = 0;
+            double promedioFallidos = 0;
+            do
             {
-                Console.WriteLine(subcadena);
+                Console.WriteLine("---------------------------");
+                Console.WriteLine("---------------------------");
+                Console.WriteLine("Por favor ingrese los valores maximo y minimo de la siguiente forma 15,20");
+                Console.WriteLine("---------------------------");
+                Console.WriteLine("---------------------------");
+                Console.WriteLine("Para teminar el ingreso de datos ingrese los valoes de la siguiente forma 0,0");
+                Console.WriteLine("---------------------------");
+                Console.WriteLine("---------------------------");
+                string cadena = Console.ReadLine();
+                subcadenas = cadena.Split(',');
+                do
+                {
+                    if (subcadenas.Length == 3)
+                    {
+                        Console.WriteLine("Solo se debe digitar 2 numeros");
+                        Console.WriteLine("Digite de nuevo");
+                        cadena = Console.ReadLine();
+                        subcadenas = cadena.Split(',');
+                    }
+                } while (subcadenas.Length == 3);
+                if (subcadenas[0] == "9" || subcadenas[1] == "9")
+                {
+                    diasFallidos++;
+                }
+                int caja1 = Convert.ToInt32(subcadenas[0]);
+                int caja2 = Convert.ToInt32(subcadenas[1]);
+                if (caja1 > caja2)
+                {
+                    int caja3;
+                    caja3 = caja1;
+                    caja1 = caja2;
+                    caja2 = caja3;
+                }
+                menor.Add(caja1);
+                mayor.Add(caja2);
+            } while (subcadenas[0] != "0" || subcadenas[1] != "0");
+            Console.WriteLine("La cantidad de dias fue:" + mayor.Count() - 1);
+            Console.WriteLine("El promedio del mayor " + mayor.Max());
+            Console.WriteLine("El promedio del menor " + menor.Max());
+            Console.WriteLine("La cantidad de dias fallido fueron:" + diasFallidos);
+            if (diasFallidos == 0)
+            {
+                promedioFallidos = 0;
             }
-
-        } while (subcadenas[0] != "0" || subcadenas[1] != "0");
-	}
+            else
+            {
+                promedioFallidos = (double)diasFallidos / (mayor.Count - 1);
+            }
+            Console.WriteLine("El porcentaje que represento fue: " + promedioFallidos);
+            Console.WriteLine("Desea ingresar un nuevo esenario s/n");
+            continua = Console.ReadLine();
+        } while (continua.Contains("s"));
+        
+    }
 }
